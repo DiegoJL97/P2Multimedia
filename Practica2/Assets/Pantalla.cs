5470302c4c6d429f1b2003aba98916fabe3f5be9
Diego <diegojaraoro@gmail.com>
2020-03-26 12:59:38 +0100

    Project

---
+++ Practica2/Assets/Pantalla.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+public class Pantalla : MonoBehaviour, ITrackableEventHandler
+{
+    TrackableBehaviour mTrackableBehaviour;
+    public VideoPlayer pantallaVideo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            pantallaVideo.Play();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            pantallaVideo.Pause();
+        }
+        else
+        {
+            pantallaVideo.Stop();
+        }
+    }
+    
+}
